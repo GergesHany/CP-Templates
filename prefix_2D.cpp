e74c0e3947ea48316d67e06b6c24c5b52d444446
Gerges Hany <105644935+GergesHany@users.noreply.github.com>
2022-12-14 02:53:16 +0200

    Update and rename prefix.cpp to prefix_2D.cpp

--- prefix_2D.cpp
+++ prefix_2D.cpp
@@ -23,5 +23,12 @@ struct prefix_sum_2d{
   ll query(vector < vector < ll > > &ps, int x1, int y1, int x2, int y2){
     return ps[x2][y2] - ps[x1 - 1][y2] - ps[x2][y1 - 1] + ps[x1 - 1][y1 - 1];
   }
+  
+  // print prefix sum 2d
+  void print(vector < vector < ll > > &ps){
+    for (int i = 1; i < sz(ps); i++)
+        for (int j = 1; j < sz(ps[0]); j++)
+            cout << ps[i][j] << " \n"[j == sz(ps[0]) - 1];
+  }
 
 };
