ab67ad854f1125aa12885e646a512d324fd0cc2d
Gerges Hany <105644935+GergesHany@users.noreply.github.com>
2022-12-15 06:33:36 +0200

    Update kadane.cpp

--- kadane.cpp
+++ kadane.cpp
@@ -1,9 +1,5 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/tree_policy.hpp>
-#include <ext/pb_ds/assoc_container.hpp>
-
 using namespace std; 
-using namespace __gnu_pbds;
 typedef long long ll;
 
 struct kadane{
