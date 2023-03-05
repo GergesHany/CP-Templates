692edfd62b0a1d44374abdcbc22c0d5c7f57c4c6
Gerges Hany <105644935+GergesHany@users.noreply.github.com>
2023-03-05 17:16:54 +0200

    Update seive.cpp

--- seive.cpp
+++ seive.cpp
@@ -12,7 +12,7 @@ struct prime{
     void Seive(int n){
         is_prime.assign(n + 1, true);
         is_prime[0] = is_prime[1] = false;
-        for(ll i = 2; i * i <= n; i++) //(i * i <= n) = (i <= sqrt(n))
+        for(ll i = 2; i * i <= n; i++) 
             if(is_prime[i])  
                 for(ll j = i * i; j <= n; j += i)
                 is_prime[j] = false;
