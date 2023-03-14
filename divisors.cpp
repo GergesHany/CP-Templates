5b5354e9e5b0a97caecaf8de14a3015b330f0773
Gerges Hany <105644935+GergesHany@users.noreply.github.com>
2023-03-14 08:22:00 +0200

    Update divisors.cpp

--- divisors.cpp
+++ divisors.cpp
@@ -1,37 +1,37 @@
 template < typename T = int > struct factorization{
 
-    // seive factorization
-    void seive_factorization(T n){ 
-      vector < T > divisors(n + 1, 2);
-        divisors[0] = 0, divisors[1] = 1;
-        for (int i = 2; i <= n / 2; i++)
-            for (int j =  i * 2; j <= n; j += i)
-              divisors[j]++;
-    }
+  // seive factorization
+  void seive_factorization(T n){ 
+    vector < T > divisors(n + 1, 2);
+      divisors[0] = 0, divisors[1] = 1;
+      for (int i = 2; i <= n / 2; i++)
+          for (int j =  i * 2; j <= n; j += i)
+            divisors[j]++;
+  }
     
-    // get the number of divisors for n
-    ll number_of_divisors(ll n){
-      ll cnt = 0;
-      for(int i = 1; i <= sqrt(n); i++){
-        if(n % i == 0){
-          cnt++;
-          if(i != n / i) cnt++;
-        }
+  // get the number of divisors for n
+  T number_of_divisors(T n){
+    T cnt = 0;
+    for(int i = 1; i <= sqrt(n); i++){
+      if(n % i == 0){
+        cnt++;
+        if(i != n / i) cnt++;
       }
-      return cnt;
     }
+    return cnt;
+  }
 
     // get Summation of divisors for n
-    ll sum_of_divisors(ll n){
-        ll sum = 0;
-        for(int i = 1; i <= sqrt(n); i++)
-            if(n % i == 0) sum += i + n / i;
-        return sum - (sqrt(n) == (ll)sqrt(n)) * sqrt(n);
+    T sum_of_divisors(T n){
+      T sum = 0;
+      for(int i = 1; i <= sqrt(n); i++)
+          if(n % i == 0) sum += i + n / i;
+      return sum - (sqrt(n) == (ll)sqrt(n)) * sqrt(n);
     }
 
     // get all divisors for n
-    vector < ll > get_divisors(ll n){
-        vector < ll > divisors;
+    vector < T > get_divisors(T n){
+        vector < T > divisors;
         for(int i = 1; i <= sqrt(n); i++){
             if(n % i == 0){
                 divisors.push_back(i);
@@ -40,12 +40,11 @@ template < typename T = int > struct factorization{
         }
     }
     
-
  // to know more for this two function :: https://www.geeksforgeeks.org/sum-divisors-1-n
 
   // function to calculate sum of all divisors from [1, n] in O(n)
-  ll sumOfDivisors(ll n) {
-    ll result = 0;
+  T sumOfDivisors(T n) {
+    T result = 0;
     for (ll i = 1; i <= n; i++) {
         result += (n / i) * i;
     }
@@ -53,13 +52,13 @@ template < typename T = int > struct factorization{
   }
 
   // function to calculate sum of all divisors from [1, n] in O(sqrt(n))
-  ll sum_divisors(ll num){
-      ll sum = 0, sq = sqrt(num);
+  T sum_divisors(T num){
+      T sum = 0, sq = sqrt(num);
       for (ll i = 1; i <= sq; i++) {
       // sum of all divisors from [i, num / i]  
-        ll add1 = i * (num / i - i + 1);
+        T add1 = i * (num / i - i + 1);
         // sum of all divisors from [num / i + 1, num] = sum of all divisors from [1, num / i] - sum of all divisors from [1, i]
-        ll add2 = (((num / i) * (num / i + 1)) / 2) - ((i * (i + 1)) / 2); 
+        T add2 = (((num / i) * (num / i + 1)) / 2) - ((i * (i + 1)) / 2); 
         sum += add1 + add2;
       }
       return sum;
