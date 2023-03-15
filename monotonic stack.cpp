cc5cdbc8ca19b4dff01b0e04dc453425411430c4
Gerges Hany <105644935+GergesHany@users.noreply.github.com>
2023-03-15 22:46:18 +0200

    Update monotonic stack.cpp

--- monotonic stack.cpp
+++ monotonic stack.cpp
@@ -8,8 +8,8 @@ template < typename T = int > struct monotonic_stack{
   T n;
   vector < T > v;
   vector < T > ans;
-  vector < pair < T, T > > st;
-
+  stack < pair < T, T > > st;
+ 
   monotonic_stack(vector < T > &v) : v(v) {
     n = sz(v);
     ans = vector < T > (n);
@@ -18,53 +18,48 @@ template < typename T = int > struct monotonic_stack{
   // function to get the next greater if no greater element return -1
   vector < T > next_greater_element(){
     for(int i = n - 1; i >= 0; i--){
-      while(!st.empty() && st.back().first <= v[i]) st.pop_back();
-      ans[i] = (st.empty() ? -1 : st.back().second);
-      st.push_back({v[i], i});
+      while(!st.empty() && st.top().first <= v[i]) st.pop();
+      ans[i] = (st.empty() ? -1 : st.top().second);
+      st.push({v[i], i});
     }
     return ans;
   }
-
+ 
   // function to get the previous greater if no greater element return -1
   vector < T > prev_greater_element(){
     for(int i = 0; i < n; i++){
-      while(!st.empty() && st.back().first <= v[i]) st.pop_back();
-      ans[i] = (st.empty() ? -1 : st.back().second);
-      st.push_back({v[i], i});
+      while(!st.empty() && st.top().first <= v[i]) st.pop();
+      ans[i] = (st.empty() ? n : st.top().second);
+      st.push({v[i], i});
     }
     return ans;
   }
-
-  // function to get the next smaller if no smaller element return -1
+ 
+  // function to get the next smaller if no smaller element return n
   vector < T > next_smaller_element(){
     for(int i = n - 1; i >= 0; i--){
-      while(!st.empty() && st.back().first >= v[i]) st.pop_back();
-      ans[i] = (st.empty() ? -1 : st.back().second);
-      st.push_back({v[i], i});
+      while(!st.empty() && st.top().first >= v[i]) st.pop();
+      ans[i] = (st.empty() ? n : st.top().second);
+      st.push({v[i], i});
     }
     return ans;
   }
-
+ 
   // funtion to get the previous smaller if no smaller element return -1
   vector < T > prev_smaller_element(){
     for(int i = 0; i < n; i++){
-      while(!st.empty() && st.back().first >= v[i]) st.pop_back();
-      ans[i] = (st.empty() ? -1 : st.back().second);
-      st.push_back({v[i], i});
+      while(!st.empty() && st.top().first >= v[i]) st.pop();
+      ans[i] = (st.empty() ? -1 : st.top().second);
+      st.push({v[i], i});
     }
     return ans;
   }
-
-
+ 
+ 
 };
 
 int main()
 {
-
-  vector < ll > v = {1, 3, 2, 4, 1};
-  monotonic stack < ll > st(v);
-  vector < ll > ans = st.next_greater_element();
-  for (auto & i: ans) cout << i << " ";
 
     
   return 0;
