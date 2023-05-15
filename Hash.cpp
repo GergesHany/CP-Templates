08212cd19a73be3ee5e37fa533ce7f4eadf89c0f
Gerges Hany <105644935+GergesHany@users.noreply.github.com>
2023-05-15 19:05:37 +0300

    Update Hash.cpp

--- Hash.cpp
+++ Hash.cpp
@@ -3,23 +3,15 @@ template < typename T = long long > struct Hash {
   T N;
   const int p[2] = {31, 37};
   const int mod[2] = {1000000007, 1000000009};
-  vector < T > h1, h2,  inv1, inv2, p_pow1, p_pow2;
- 
- 
-  T bpow(T n, T x, T m) {
-    return !x ? 1 : 1LL * bpow(n * n % m, x >> 1, m) * (x & 1 ? n : 1) % m;
-  }
+  vector < T > h1, h2,  p_pow1, p_pow2;
  
   Hash(T N){
     this -> N = N;
-    h1 = h2 = inv1 = inv2 = p_pow1 = p_pow2 = vector < T > (N);
-    p_pow1[0] = p_pow2[0] = 1, inv1[1] = inv2[1] = 1;
-    ll inv_p1 = bpow(p[0], mod[0] - 2, mod[0]), inv_p2 = bpow(p[1], mod[1] - 2, mod[1]);
+    h1 = h2 = p_pow1 = p_pow2 = vector < T > (N);
+    p_pow1[0] = p_pow2[0] = 1;
     for (T i = 1; i < N; i++) {
       p_pow1[i] = (1LL * p_pow1[i - 1] * p[0]) % mod[0];
       p_pow2[i] = (1LL * p_pow2[i - 1] * p[1]) % mod[1];
-      inv1[i] = (1LL * inv1[i - 1] * inv_p1) % mod[0];
-      inv2[i] = (1LL * inv2[i - 1] * inv_p2) % mod[1];
     }
   }
  
