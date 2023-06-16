a95fcf5c24c4d63152e9923d83d0ecbe4e4f850d
Gerges Hany <105644935+GergesHany@users.noreply.github.com>
2023-06-16 14:04:45 +0300

    Update Sieve Prime Factors.cpp

--- Sieve Prime Factors.cpp
+++ Sieve Prime Factors.cpp
@@ -1,59 +1,56 @@
-#include <bits/stdc++.h> 
-using namespace std; 
-
+template < typename T = int > struct Sieve_of_Eratosthenes{
   
-typedef long long ll;
+  Sieve_of_Eratosthenes() = default;
 
-// spf to store the smallest prime factor for each number from 1 to N,
-vector < ll > spf; 
+  Sieve_of_Eratosthenes(T N, bool store = false){
+    sieve(N);
+    if(store){
+      PrimeFactors = vector < vector < T > > (N + 5);
+      for(int i = 1; i <= N; i++){
+        PrimeFactors[i] = Prime_Factors(i);
+      }
+    }
+  }
 
-// PrimeFactors to store the prime factors of each number from 1 to N.
-vector < vector < ll > > PrimeFactors; 
+  // spf to store the smallest prime factor for each number from 1 to N,
+  vector < ll > spf; 
 
-// calculate the prime factors of N. 
-vector < ll > Prime_Factors(ll N){
-    vector < ll > ret;
+  // PrimeFactors to store the prime factors of each number from 1 to N.
+  vector < vector < ll > > PrimeFactors; 
+
+  void sieve(T N){
+    
+    spf = vector < T > (N + 5, 0);
+    spf[0] = spf[1] = 1; // 0 and 1 are not prime numbers. they are special cases.
+
+    // Marking smallest prime factor for every number to be itself.
+    for(int i = 2; i < N; i++) spf[i] = i;
+
+    // separately marking spf for every even number as 2. 
+    for (int i = 2; i < N; i += 2) spf[i] = 2;
+        
+    // Iterating over all odd numbers.
+    for (T i = 3; i * i < N; i++) { 
+      if (spf[i] == i) { 
+        for (T j = i * i; j < N; j += i) 
+          if (spf[j] == j) 
+            spf[j] = i; 
+      } 
+    }    
+
+  }
+
+  vector < T > Prime_Factors(T N){
+    vector < T > ret;
     while(N > 1){
       ret.push_back(spf[N]);
       N /= spf[N];
     }
     return ret;
-}
+  }
 
-void sieve(ll N){
-  
-  spf = vector < ll > (N + 5, 0);
-  PrimeFactors = vector < vector < ll > > (N + 5);
+  vector < T > Factors(T n){
+    return PrimeFactors[n];
+  }
 
-  spf[0] = spf[1] = 1; // 0 and 1 are not prime numbers. they are special cases.
-
-  // Marking smallest prime factor for every number to be itself.
-  for(int i = 2; i < N; i++) spf[i] = i;
-
-  // separately marking spf for every even number as 2. 
-  for (int i = 2; i < N; i += 2) spf[i] = 2;
-      
-  // Iterating over all odd numbers.
-  for (ll i = 3; i * i < N; i++) { 
-    if (spf[i] == i) { 
-      for (ll j = i * i; j < N; j += i) 
-        if (spf[j] == j) 
-          spf[j] = i; 
-    } 
-  }    
-
-  // Calculating the prime factors of each number from 1 to N.
-  for (ll i = 1; i < N; i++)
-    PrimeFactors[i] = Prime_Factors(i);
-
-}
-
-
-int main()
-{ 
-
-
-
-  return 0;
-}
- 
+};
