7bd1df22e0cc86f1e62ca19bae17ddbfde47c7af
Gerges Hany <105644935+GergesHany@users.noreply.github.com>
2023-07-05 18:30:20 +0300

    Update Bridges.cpp

--- Bridges.cpp
+++ Bridges.cpp
@@ -6,19 +6,23 @@ template < typename T = int > struct Bridges{
   vector < pair < T, T > > bridges;
   vector < T > dfs_num, dfs_low, vis;
 
+  // default constructor
   Bridges() = default;
 
+  // constructor to initialize the graph
   Bridges(T n){
     dfs_timer = 0;
     adj = vector < vector < T > > (n + 1);
     dfs_num = dfs_low = vis = vector < T > (n + 1);
   }
 
+  // add edge to the graph
   void add_edge(T u, T v, bool bidirectional = true){
     adj[u].push_back(v);
     if (bidirectional) adj[v].push_back(u);
   }
 
+  // tarjan algorithm to find bridges
   void tarjan(T node, T parent){
     dfs_num[node] = dfs_low[node] = ++dfs_timer;
     vis[node] = true;
@@ -27,7 +31,7 @@ template < typename T = int > struct Bridges{
       if (!dfs_num[child]){
         tarjan(child, node);
         dfs_low[node] = min(dfs_low[node], dfs_low[child]);
-        if (dfs_low[child] > dfs_num[node]){
+        if (dfs_low[child] > dfs_num[node]){ // bridge found
           bridges.push_back({node, child});
         }
       }else if (vis[child]){
@@ -39,7 +43,8 @@ template < typename T = int > struct Bridges{
   void solve(){
     for (T i = 1; i < sz(adj); ++i) if (!dfs_num[i]) tarjan(i, -1);
   }
-
+ 
+  // print the bridges
   void print(){
     cout << sz(bridges) << "\n";
     for (auto &x : bridges){
@@ -47,6 +52,7 @@ template < typename T = int > struct Bridges{
     }
   }
 
+  // return the bridges
   vector < pair < T, T > > get_bridges(){
     return bridges;
   }
