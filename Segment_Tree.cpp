ebfdd3c1b8e8b24eb0e5dfa732d715ee83f20c94
Gerges Hany <105644935+GergesHany@users.noreply.github.com>
2023-08-01 21:12:15 +0300

    Add files via upload

---
+++ Segment_Tree.cpp
@@ -0,0 +1,144 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+  
+using namespace std; 
+using namespace __gnu_pbds;
+  
+#define EPS 1e-8
+#define sz(s) (int)(s.size())  
+#define rall(s)  s.rbegin(), s.rend()
+#define getline(s) getline(cin >> ws, s)
+#define all(vec)  vec.begin(),  vec.end()
+#define Num_of_Digits(n) ((int)log10(n) + 1)
+#define endd(s) return void(cout << s << "\n")
+#define fixed(n) cout << fixed << setprecision(n)
+#define ceill(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
+#define Time cerr << "Time Taken: " << (float)clock() / CLOCKS_PER_SEC << " Secs" << "\n";
+#define ordered_set tree<ll, null_type, less <ll>, rb_tree_tag,tree_order_statistics_node_update>
+#define multi_ordered_set tree<ll, null_type, less_equal <ll>, rb_tree_tag,tree_order_statistics_node_update>
+  
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+const ll mod = 1e9 + 7, INF = 1LL << 60, N = 3000 + 5;
+  
+void Gerges_Hany(){
+  ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+  #ifndef ONLINE_JUDGE  
+    freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout);
+  #endif
+}
+  
+template < typename T = int > istream& operator >> (istream &in, vector < T > &v) {
+  for (auto &x : v) in >> x; return in;
+}
+  
+template < typename T = int > ostream& operator << (ostream &out, const vector < T > &v) { 
+  for (const T &x : v) out << x << ' '; return out;
+}
+
+struct Item {
+
+  ll val;
+
+  Item(ll val = 0) : val(val) {}  
+
+  Item operator = (const ll rhs) {
+    val = rhs;
+    return *this;
+  }
+
+};
+
+template < typename T = int , bool Base = 0 > struct Segment_Tree {
+
+  T size; 
+  Item DEFAULT;
+  vector < Item > Tree; 
+  
+  Segment_Tree(T n, const vector < T >& vec, T Default = 0){
+    size = 1;
+    DEFAULT = Item(Default);
+    while(size < n) size <<= 1;
+    Tree = vector < Item > (size << 1, DEFAULT);
+    build(vec);
+  }
+
+  Item operation(const Item& a, const Item& b){
+    return a.val > b.val ? a : b;
+  }
+  
+  void build(const vector < T >& vec, int idx, int L, int R){
+    if ((Base && L >= sz(vec)) || (!Base && L > sz(vec))) return;
+    if(R == L) return void(Tree[idx] = vec[L - !Base]);
+    int mid = (R + L) / 2;
+    build(vec, idx << 1 , L, mid);
+    build(vec, idx << 1 | 1, mid + 1, R);
+    Tree[idx] = operation(Tree[idx << 1], Tree[idx << 1 | 1]);
+  }
+
+  void build(const vector < T >& vec){
+    build(vec, 1, 1, size);
+  }
+
+  void update(int index, T value, int idx, int L, int R){
+    if(R == L) return void(Tree[idx] = value);
+    int mid = (R + L) / 2;
+    if(index <= mid) update(index, value, idx << 1, L, mid);
+    else update(index, value, idx << 1 | 1, mid + 1, R);
+    Tree[idx] = operation(Tree[idx << 1], Tree[idx << 1 | 1]);
+  }
+
+  void update(const int index, const T value){
+    update(index, value, 1, 1, size);
+  }
+ 
+  Item query(int L, int R, int idx, int lq, int rq){
+    if(lq > R || L > rq) return DEFAULT;
+    if(lq >= L && rq <= R) return Tree[idx];
+    int mid = (lq + rq) / 2;
+    return operation(query(L, R, idx << 1, lq, mid), query(L, R, idx << 1 | 1, mid + 1, rq));
+  }
+ 
+  T query(const int l, const int r){
+    return query(l, r, 1, 1, size).val;
+  }
+
+  friend ostream& operator << (ostream &out, const Item &item) {
+    out << item.val;
+    return out;
+  }
+
+  // Segment_Tree < ll > st(size of vector, vector, Default value);
+
+};
+
+
+
+void Accepted(){
+  
+  int n;
+  cin >> n;
+  vector < ll > v(n);
+  cin >> v;
+  Segment_Tree < ll > st(n, v, -INF);
+  cout << st.query(1, n) << '\n';
+  st.update(n, 0);
+  cout << st.query(1, n) << '\n';
+
+}
+
+int main()
+{ 
+
+  Gerges_Hany();
+  int testcaces = 1, T = 1;
+  // cin >> testcaces;
+  while (testcaces--){
+    // cout << "Case " << T++ << ": ";
+    Accepted();
+  }   
+  Time;
+  return 0;
+}
