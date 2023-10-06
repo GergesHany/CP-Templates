16ae1179adf678e0efdfa6957dff87a616f25be6
Gerges Hany <105644935+GergesHany@users.noreply.github.com>
2023-10-06 16:13:41 +0300

    Update Sieve of Eratosthenes.cpp

--- Math and Number Theory/Sieve of Eratosthenes.cpp
+++ Math and Number Theory/Sieve of Eratosthenes.cpp
@@ -12,24 +12,12 @@ template < typename T = int > struct Sieve_of_Eratosthenes{
     }
   }
 
-  // spf to store the smallest prime factor for each number from 1 to N,
   vector < ll > spf; 
-
-  // PrimeFactors to store the prime factors of each number from 1 to N.
   vector < vector < ll > > PrimeFactors; 
 
   void sieve(T N){
-    
     spf = vector < T > (N + 5, 0);
-    spf[0] = spf[1] = 1; // 0 and 1 are not prime numbers. they are special cases.
-
-    // Marking smallest prime factor for every number to be itself.
-    for(int i = 2; i < N; i++) spf[i] = i;
-
-    // separately marking spf for every even number as 2. 
-    for (int i = 2; i < N; i += 2) spf[i] = 2;
-        
-    // Iterating over all odd numbers.
+    for (int i = 1; i < N; i++) spf[i] = (i & 1 ? 1 : 2);
     for (T i = 3; i * i < N; i++) { 
       if (spf[i] == i) { 
         for (T j = i * i; j < N; j += i) 
@@ -37,7 +25,6 @@ template < typename T = int > struct Sieve_of_Eratosthenes{
             spf[j] = i; 
       } 
     }    
-
   }
 
   vector < T > Prime_Factors(T N){
