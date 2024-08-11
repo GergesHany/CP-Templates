68e8a6043b98cf13f2d81331066cec76e2dd1bf6
GergesHany <gergeshany179@gmail.com>
2024-08-11 22:13:25 +0300

    update Trie & Binary_Trie

--- Strings Algorithm/Binary-Trie.cpp
+++ Strings Algorithm/Binary-Trie.cpp
@@ -1,75 +1,166 @@
+#include <bits/stdc++.h>
+ 
+using namespace std; 
+#define PI acos(-1)
+#define sz(s) (int)(s.size())  
+#define rall(s)  s.rbegin(), s.rend()
+#define ceill(x, y)  ((x + y - 1) / y)
+#define all(vec)  vec.begin(), vec.end()
+#define Time cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+ 
+int tt, tc;
+typedef long long ll;
+typedef long double ld;
+const ll INF = 1LL << 60;
+const int mod = 1e9 + 7, N = 1e6 + 5;
 
-template < int LOG = 0 > struct Trie{
+template < typename T = int > istream& operator >> (istream &in, vector < T > &v) {
+  for (auto &x : v) in >> x; return in;
+}
   
-  char base_char[4] = {'a', 'A', '0', '0'};
-  constexpr static int sz[4] = {26, 26, 10, 2}; 
+template < typename T = int > ostream& operator << (ostream &out, const vector < T > &v) { 
+  for (const T &x : v) out << x << ' '; return out;
+}
+
+template < typename T = int > class Binary_Trie{
+public:
+ 
+  Binary_Trie(int log = 31) : root(new Node()), LOG(log) {}
+ 
+  inline bool get_bit(const T x, const int bit) const {
+    return (x >> bit) & 1;
+  }
+ 
+  inline void insert(const T x) {
+    add(x);
+  }
+ 
+  inline void erase(const T x) {
+    assert(search(x)); // element must be present in the trie
+    erase(x, LOG, root);
+  }
+ 
+  inline bool search(const T x) {
+    return find(x);
+  }
+ 
+  inline T max_xor(const T x) {
+    return get_max_xor(x);
+  }
+ 
+  inline T min_xor(const T x) {
+    return ((1LL << LOG) - 1) ^ get_max_xor(x ^ ((1LL << LOG) - 1));
+  }
+ 
+  inline T count(const T x, const T k) {
+    return count_less_equal(x, k);
+  }
   
-  struct node{
+  inline T count_greater(const T x, const T k) {
+    return cnt - count_less_equal(x, k);
+  }
+ 
+  ~Binary_Trie(){
+    function < void(Node*) > dfs = [&](Node* cur){
+      if (!cur) return;
+      for (int i = 0; i < 2; ++i) dfs(cur -> child[i]);
+      delete cur;
+    };
+    dfs(root);
+  }
+ 
+private:
   
+  struct Node {
     int freq;
-    bool isval;
-    node* child[sz[3]];
-    
-    node(){
-      freq = 0;
-      isval = false;
-      memset(child, 0, sizeof(child));
-    }
-  }; 
+    Node* child[2];
+    Node() : freq(0), child{nullptr, nullptr} {}
+  };
  
-  node* root;
+  Node* root;
+  int LOG, cnt;
  
-  Trie(){
-    root = new node;
-  }
- 
-  void insert(int val){
-    node* current = root;
-    for(int bit = LOG; bit >= 0; bit--){
-       ll bit_val = (val >> bit) & 1;
-        if (current -> child[bit_val] == nullptr) 
-          current -> child[bit_val] = new node;
-        current = current -> child[bit_val];
-      current -> freq++;
+  inline void add(const T x){
+    ++cnt; // inc number of elements in the trie
+    Node* cur = root;
+    for(int i = LOG; ~i; i--){
+      bool bit = get_bit(x, i);
+      if(!cur -> child[bit]) 
+          cur -> child[bit] = new Node();
+      cur = cur -> child[bit];
+      cur -> freq++;
     }
   }
  
-  void erase(const ll val, int bit, node* curr){
-    if (bit == -1) return void(curr -> isval = curr -> freq > 1);
-    ll bit_val = (val >> bit) & 1;
-    erase(val, bit - 1, curr -> child[bit_val]);
-    if (--curr -> child[bit_val] -> freq == 0){
+  void erase(const T x, const int bit, Node* curr) {
+    if(bit < 0) return void(--cnt);
+    bool bit_val = get_bit(x, bit);
+    erase(x, bit - 1, curr -> child[bit_val]);
+    if (--curr -> child[bit_val] -> freq == 0) {
       delete curr -> child[bit_val];
       curr -> child[bit_val] = nullptr;
     }
   }
-
-  bool search(ll val){
-    node* current = root;
-    for(int bit = LOG; bit >= 0; bit--){
-      ll bit_val = (val >> bit) & 1;
-      if (current -> child[bit_val] == nullptr) return false;
-      current = current -> child[bit_val];
+ 
+  inline bool find(const T x){
+    Node* cur = root;
+    for(int i = LOG; ~i; i--){
+      bool bit = get_bit(x, i);
+      if(!cur -> child[bit] or !cur -> child[bit] -> freq) 
+          return false;
+      cur = cur -> child[bit];
     }
-    return current -> isval;
-  }
-
-  void erase(ll val){
-     erase(val, LOG, root);
+    return true;
   }
  
-  ll max_xor(ll val){
-    ll ans = 0;
-    node* current = root;
-    for(int bit = LOG; bit >= 0; bit--){
-      ll bit_val = (val >> bit) & 1;
-      if (current -> child[bit_val ^ 1] != nullptr){
-        ans += (1LL << bit);
-        current = current -> child[bit_val ^ 1];
-      }
-      else current = current -> child[bit_val];
+  inline T get_max_xor(const T x){
+    T ans = 0;
+    Node* cur = root;
+    for(int i = LOG; ~i; i--){
+      bool bit = get_bit(x, i);
+      if(cur -> child[!bit] and cur -> child[!bit] -> freq){
+        ans |= (1LL << i);
+        cur = cur -> child[!bit];
+      }else cur = cur -> child[bit];
     }
     return ans;
   }
  
+  // number of pairs such that (p ^ val) <= k
+  inline T count_less_equal(const T x, const T k){
+    T ans = 0;
+    Node* cur = root;
+    for(int i = LOG; ~i; i--){
+      if(!cur) break;
+      bool val_bit = get_bit(x, i), k_bit = get_bit(k, i);
+      if(k_bit){
+        if(cur -> child[val_bit]) 
+            ans += cur -> child[val_bit] -> freq;
+        cur = cur -> child[val_bit ^ 1];
+      }else cur = cur -> child[val_bit];
+    }
+    if(cur) ans += cur -> freq;
+    return ans;
+  }
+ 
 };
+
+void Accepted(){
+  
+  
+ 
+} 
+
+int main(){ 
+
+  ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+  if (fopen("input.txt", "r")) {
+    freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout); 
+  }
+
+  tt = tc = 1; // cin >> tt;
+  while (tt--) Accepted(), ++tc;  
+
+  Time;
+  return 0;
+}
--- Strings Algorithm/Trie.cpp
+++ Strings Algorithm/Trie.cpp
@@ -1,85 +1,139 @@
-template < int base = 0 > struct Trie{
+#include <bits/stdc++.h>
+ 
+using namespace std; 
+#define PI acos(-1)
+#define sz(s) (int)(s.size())  
+#define rall(s)  s.rbegin(), s.rend()
+#define ceill(x, y)  ((x + y - 1) / y)
+#define all(vec)  vec.begin(), vec.end()
+#define Time cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+ 
+int tt, tc;
+typedef long long ll;
+typedef long double ld;
+const ll INF = 1LL << 60;
+const int mod = 1e9 + 7, N = 1e6 + 5;
+
+template < typename T = int > istream& operator >> (istream &in, vector < T > &v) {
+  for (auto &x : v) in >> x; return in;
+}
   
-  char base_char[4] = {'a', 'A', '0', '0'};
-  constexpr static int sz[4] = {26, 26, 10, 2}; 
+template < typename T = int > ostream& operator << (ostream &out, const vector < T > &v) { 
+  for (const T &x : v) out << x << ' '; return out;
+}
 
-  struct node{
-  
-    int freq;
-    bool isWord;
-    node* child[sz[base]];
-    
-    node(){
-      freq = 0;
-      isWord = false;
-      memset(child, 0, sizeof(child));
-    }
-  }; 
+enum class TrieMode { Lowercase, Uppercase, Digits }; // from 7oskaa's Trie
+template < TrieMode Mode = TrieMode::Lowercase > class Trie{
+public:
 
-  node* root;
+  Trie() : root(new Node()) {}
+ 
+  inline const void insert(const string& s) {
+    add(s);
+  }
 
-  Trie(){
-    root = new node();
+  inline const void erase(const string& s){
+    remove(s, 0, root);
+  }
+
+  inline const bool search(const string& s){
+    return find(s);
+  }
+
+  inline const bool is_prefix(const string& s){
+    return prefix(s);
   }
 
   ~Trie(){
-    Delete(root);
-    delete root;
+    function < void(Node*) > dfs = [&](Node* cur){
+      if (!cur) return;
+      for (int i = 0; i < get_size(); ++i) dfs(cur -> child[i]);
+      delete cur;
+    };
+    dfs(root);
+  }  
+ 
+private:
+  static constexpr int get_size(){ 
+    if constexpr (Mode == TrieMode::Digits) return 10;
+    if constexpr (Mode == TrieMode::Lowercase) return 26;
+    if constexpr (Mode == TrieMode::Uppercase) return 26;
+    assert(false); // unreachable
+  }
+  
+  inline constexpr int get_index(char c){
+    if constexpr (Mode == TrieMode::Digits) return c - '0';
+    if constexpr (Mode == TrieMode::Lowercase) return c - 'a';
+    if constexpr (Mode == TrieMode::Uppercase) return c - 'A';
+    assert(false); // unreachable
   }
 
-  void Delete(node* current){
-    for (auto Node : current -> child){
-       if (Node != NULL) {
-          Delete(Node);
-          delete Node;
-       }
+  struct Node {
+    int freq = 0;
+    bool is_word = false;
+    Node* child[get_size()] = {nullptr};
+  };
+
+  Node* root;
+
+  inline void add(const string& s){
+    Node* cur = root;
+    for (char c : s) {
+      int index = get_index(c);
+      if (!cur -> child[index]) cur -> child[index] = new Node();
+      cur = cur -> child[index];
     }
+    cur -> is_word = true;
   }
 
-  void insert(string s){
-    node* current = root;
-    for(auto c : s){
-      int idx = c - base_char[base];
-      if (current -> child[idx] == NULL) 
-        current -> child[idx] = new node;
-      current = current -> child[idx];
-      current -> freq++;
+  inline bool find(const string& s){
+    const Node* cur = root;
+    for (char c : s) {
+      int index = get_index(c);
+      if (!cur->child[index]) return false;
+      cur = cur->child[index];
     }
-    current -> isWord = true;
+    return cur->is_word;
   }
 
-  bool search(string s){
-    node* current = root;
-    for (auto c : s){
-      int idx = c - base_char[base];
-      if (current -> child[idx] == NULL) return false;
-      current = current -> child[idx];
-    }
-    return current -> isWord;
-  }
-
-  bool is_prefix(string s){
-    node* current = root;
-    for (auto c : s){
-      int idx = c - base_char[base];
-      if (current -> child[idx] == NULL) return false;
-      current = current -> child[idx];
+  inline bool prefix(const string& s){
+    const Node* cur = root;
+    for (char c : s) {
+      int index = get_index(c);
+      if (!cur->child[index]) return false;
+      cur = cur->child[index];
     }
     return true;
   }
 
-  void erase(const string& word, int idx, node* current){
-    if(idx == sz(word)) return void(current -> isWord = current -> freq != 1);
-    erase(word, idx + 1, current -> child[word[idx] - base_char[base]]);
-    if(--current -> child[word[idx] - base_char[base]] -> freq == 0){
-        delete current -> child[word[idx] - base_char[base]];
-        current -> child[word[idx] - base_char[base]] = nullptr;
+  void remove(const string& s, int i, const Node* cur){
+    if (i == sz(s)) return void(cur -> is_word = false);
+    int index = get_index(s[i]);
+    assert(cur -> child[index]); // check if the character exists
+    remove(s, i + 1, cur -> child[index]);
+    if (cur->child[index] -> freq == 0) {
+      delete cur->child[index];
+      cur->child[index] = nullptr;
     }
   }
+ 
+};
+
+void Accepted(){
   
-  void erase(string s){
-    if (search(s))
-     erase(s, 0, root);
+
+} 
+
+int main(){ 
+
+  ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+  if (fopen("input.txt", "r")) {
+    freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout); 
   }
 
-};
+  tt = tc = 1; // cin >> tt;
+  while (tt--) Accepted(), ++tc;  
+
+  Time;
+  return 0;
+}
