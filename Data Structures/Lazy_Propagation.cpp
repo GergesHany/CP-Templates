238e94f437d9efc58ae1600b9de585d40086b85e
GergesHany <gergeshany179@gmail.com>
2024-10-11 18:25:01 +0300

    update Lazy_Propagation

--- Data Structures/Lazy_Propagation.cpp
+++ Data Structures/Lazy_Propagation.cpp
@@ -1,34 +1,20 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/tree_policy.hpp>
-#include <ext/pb_ds/assoc_container.hpp>
-
+ 
 using namespace std; 
-using namespace __gnu_pbds;
-
-#define EPS 1e-8
+#define PI acos(-1)
+// #define int long long
 #define sz(s) (int)(s.size())  
 #define rall(s)  s.rbegin(), s.rend()
-#define getline(s) getline(cin >> ws, s)
-#define all(vec)  vec.begin(),  vec.end()
-#define Num_of_Digits(n) ((int)log10(n) + 1)
-#define endd(s) return void(cout << s << "\n")
-#define fixed(n) cout << fixed << setprecision(n)
-#define ceill(n, m) (((n) / (m)) + ((n) % (m) ? 1 : 0))
-#define Time cerr << "Time Taken: " << (float)clock() / CLOCKS_PER_SEC << " Secs" << "\n";
-
-int tt, tc;  
+#define ceill(x, y)  ((x + y - 1) / y)
+#define all(vec)  vec.begin(), vec.end()
+#define Time cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+ 
+int tt, tc;
 typedef long long ll;
 typedef long double ld;
-typedef unsigned long long ull;
-const ll mod = 1e9 + 7, INF = 1LL << 60, N = 2e3 + 5;
-  
-void Gerges_Hany(){
-  ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
-  #ifndef ONLINE_JUDGE  
-    freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout);
-  #endif
-}
-  
+const ll INF = 1LL << 60;
+const int mod = 1e9 + 7, N = 1e6 + 5;
+
 template < typename T = int > istream& operator >> (istream &in, vector < T > &v) {
   for (auto &x : v) in >> x; return in;
 }
@@ -37,137 +23,153 @@ template < typename T = int > ostream& operator << (ostream &out, const vector <
   for (const T &x : v) out << x << ' '; return out;
 }
 
-struct Node {
-  
-  bool is_lazy;
-  ll val, update;
-  
-  Node(ll V = 0) : val(V), update(false), is_lazy(false) {}
+/**
+ * Represents the type of lazy update being done.
+ * NONE = if there's no lazy update to be performed.
+*/
 
-  Node operator = (const ll &other){
-    this -> val = other;
-    return *this;
-  }
-};
+enum QueryType { ADD, SET, NONE };
+struct Query {
 
-template < typename T = int, const bool Base = 0 > struct Lazy_Propagation {
-  
-  int size;
-  vector < Node > Tree;
-  T query_default, init_default;
+  QueryType type = NONE;
+  ll val = 0;
 
-  #define LEFT (idx << 1)
-  #define RIGHT ((idx << 1) | 1)
-  
-  // the function that will be used to update the Tree
-  function < T(T, T) > Lazy_operation;
-
-  // the function that will be used to query on the Tree
-  function < Node(Node, Node) > Tree_operation;
- 
-  inline void intial(int n, T qd = 0, T id = 0){
-    size = 1;
-    while(size <= n) size *= 2;
-    query_default = qd, init_default = id;
-    Tree = vector < Node > (2 * size, Node(init_default));
-  }
- 
-  explicit Lazy_Propagation(int n){
-    intial(n);
-  }
- 
-  inline void propagate(int idx, int lx, int rx){
-    if(!Tree[idx].is_lazy) return;
-    Tree[idx].val = Lazy_operation(Tree[idx].val, (rx - lx + 1) * Tree[idx].update);
-    if(lx != rx){
-      Tree[LEFT].update = Lazy_operation(Tree[LEFT].update, Tree[idx].update);
-      Tree[RIGHT].update = Lazy_operation(Tree[RIGHT].update, Tree[idx].update);
-      Tree[LEFT].is_lazy = Tree[RIGHT].is_lazy = true;
-    }
-    Tree[idx].update = init_default, Tree[idx].is_lazy = false;
-  }
- 
-  inline void update_lazy(int idx, T v){
-    Tree[idx].update = Lazy_operation(Tree[idx].update, v);
-    Tree[idx].is_lazy = true;
-  }
- 
-  inline void build(const vector < T >& nums, int idx, int lx, int rx){
-    propagate(idx, lx, rx);
-    if(Base ? lx >= sz(nums) : lx > sz(nums)) return;
-    if(rx == lx) Tree[idx] = nums[lx - !Base];
-    else {
-      int mx = (rx + lx) / 2;
-      build(nums, LEFT, lx, mx), build(nums, RIGHT, mx + 1, rx);
-      propagate(LEFT, lx, mx), propagate(RIGHT, mx + 1, rx);
-      Tree[idx] = Tree_operation(Tree[LEFT], Tree[RIGHT]);
-    }
-  }
- 
-  inline void build(const vector < T >& nums){
-    build(nums, 1, 1, size);
-  }
- 
-  inline void update(int l, int r, T v, int idx, int lx, int rx){
-    propagate(idx, lx, rx);
-    if(lx >= l && rx <= r) return update_lazy(idx, v);
-    if(lx > r || rx < l) return;
-    int mx = (lx + rx) / 2;
-    update(l, r, v, LEFT, lx, mx), update(l, r, v, RIGHT, mx + 1, rx);
-    propagate(LEFT, lx, mx), propagate(RIGHT, mx + 1, rx);
-    Tree[idx] = Tree_operation(Tree[LEFT], Tree[RIGHT]);
-  }
- 
- 
-  inline void update(int l, int r, T v){
-    update(l, r, v, 1, 1, size);
-  }
- 
-  Node query(int l, int r, int idx, int lx, int rx){
-    propagate(idx, lx, rx);
-    if(lx >= l && rx <= r) return Tree[idx];
-    if(lx > r || rx < l) return query_default;
-    int mx = (rx + lx) / 2;
-    propagate(LEFT, lx, mx), propagate(RIGHT, mx + 1, rx);
-    return Tree_operation(query(l, r, LEFT, lx, mx), query(l, r, RIGHT, mx + 1, rx));
-  }
- 
-  inline T query(int l, int r){
-    return query(l, r, 1, 1, size).val;
-  }
- 
-  #undef LEFT
-  #undef RIGHT
- 
 };
 
 /*
-  
-  function < ll(ll, ll) > Lazy_operation = [&](ll a, ll b) -> ll { return b; };
-  function < Node(Node, Node) > Tree_operation = [&](Node a, Node b) -> Node {
-    return Node(a.val + b.val);
-  };
 
-  Lazy_Propagation < ll > seg(n);
-  seg.Lazy_operation = Lazy_operation;
-  seg.Tree_operation = Tree_operation;
-  seg.build(a);
+ * T = tree node type, which will be long long
+ * We use the Query struct to manage updates.
 
 */
+
+template < class T > class Lazysegment_tree {
+
+private:
+  int size;
+  T DEFAULT = 0;
+  vector < Query > lazy;  // lazy[i] = lazy update for the range
+  vector < T > tree;      // tree[i] = Tree_operation of this node's range
+
+
+  /** @return result of joining two tree nodes together */
+
+  inline T Tree_operation(T a, T b) { return a + b; }
+
+  /** builds the segtree nodes */
+  void build(int v, int l, int r, const vector<T> &a) {
+    if (l == r) {
+      tree[v] = a[l];
+    } else {
+      int m = (l + r) / 2;
+      build(2 * v, l, m, a);
+      build(2 * v + 1, m + 1, r, a);
+      tree[v] = Tree_operation(tree[2 * v], tree[2 * v + 1]);
+    }
+  }
+
+  /** applies lazy update to t[v], places update at lz[v] */
+  void apply(int v, int len, const Query &x) {
+    if (x.type == ADD) {
+      // if lazy[v]'s type is NONE or ADD, then we add to the range
+      // otherwise, we add to our lazy set value
+      if (lazy[v].type != SET) {
+        lazy[v] = Query{ADD, lazy[v].val + x.val};
+      } else {
+        lazy[v] = Query{SET, lazy[v].val + x.val};
+      }
+       tree[v] += x.val * len;
+      } else if (x.type == SET) {
+        // lazy set overrides any previous update
+        tree[v] = x.val * len;
+        lazy[v] = x;
+     }
+  }
+
+  /** pushes down lazy update to children of v */
+  void push_down(int v, int l, int r) {
+    if (lazy[v].type != NONE && l != r) {
+      int m = (l + r) / 2;
+      apply(2 * v, m - l + 1, lazy[v]);
+      apply(2 * v + 1, r - m, lazy[v]);
+    }
+    lazy[v] = Query{NONE, 0};
+  }
+
+
+  void range_update(int v, int l, int r, int ql, int qr, const Query &x) {
+    if (qr < l || ql > r) return;
+    if (ql <= l && r <= qr) {
+      apply(v, r - l + 1, x);
+    } else {
+      push_down(v, l, r);
+      int m = (l + r) / 2;
+      range_update(2 * v, l, m, ql, qr, x);
+      range_update(2 * v + 1, m + 1, r, ql, qr, x);
+      tree[v] = Tree_operation(tree[2 * v], tree[2 * v + 1]);
+    }
+  }
+
+  T range_sum(int v, int l, int r, int ql, int qr) {
+    if (qr < l || ql > r) return DEFAULT; 
+    if (l >= ql && r <= qr) return tree[v];
+    push_down(v, l, r);
+    int m = (l + r) / 2;
+    auto L = range_sum(2 * v, l, m, ql, qr);
+    auto R = range_sum(2 * v + 1, m + 1, r, ql, qr);
+    return Tree_operation(L, R);
+  }
+
+public:
+  inline void init(const int n){
+    size = 1;
+    while (size < n) size *= 2;
+    tree.assign(2 * size, 0);
+    lazy.assign(2 * size, Query{NONE, 0});
+  }
+
+  Lazysegment_tree(const int n, T DEFAULT_ = 0) : DEFAULT(DEFAULT_) {
+    init(n);
+  }
+
+  Lazysegment_tree(const vector < T > &a, T DEFAULT_ = 0) : DEFAULT(DEFAULT_) {
+    init(sz(a));
+    build(1, 0, size - 1, a);
+  }
+
+  /** updates [ql, qr] with the update x */
+  inline void range_update(int ql, int qr, const Query &x) {
+    range_update(1, 0, size - 1, ql, qr, x);
+  }
+
+  /** sum of array values on [ql, qr] */
+  inline T query(int ql, int qr) { return range_sum(1, 0, size - 1, ql, qr); }
+
+  inline T operator[](int i) { return query(i, i); }
+  
+  // ** add and update range [l, r] to x */
+  inline void add(int l, int r, T x) { range_update(l, r, Query{ADD, x}); }
+  inline void update(int l, int r, T x) { range_update(l, r, Query{SET, x}); }
+
+  // ** add and update single index i to x */
+  inline void add(int i, T x) { add(i, i, x); }
+  inline void update(int i, T x) { update(i, i, x); }
+
+};
+
 
 void Accepted(){
   
 
-}
+} 
 
-int main(){ 
+signed main(){ 
 
-  tt = 1;
-  Gerges_Hany();
-  // cin >> tt;
-  while(tt--) 
-    Accepted();
-  
+  ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+  tt = tc = 1; // cin >> tt;
+  while (tt--) Accepted(), ++tc;  
+
   Time;
   return 0;
 }
